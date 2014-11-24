0d953666c4a0249dc94b92102055b3f828b82736
wernerkuhn <kuhn@spatial.ucsb.edu>
2014-11-24 12:11:33 -0800

    new versions of the 5 core concept specs
    changed class names to singular, cleaned up Location, removed
    unnecessary code

--- CoreConceptsHs/Event.hs
+++ CoreConceptsHs/Event.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses #-} 
 -- core concept: event 
-<<<<<<< HEAD
 -- core questions: when did this event happen? what properties and relations does the event have? what participants does it have?
 -- events are process portions with fields, objects, and networks as participants
 -- conjecture: all events can be seen as movements at some granularity
@@ -11,18 +10,6 @@
 -- computing event outcomes: central, but how to specify? do simple examples
 -- (c) Werner Kuhn
 -- latest change: Nov 23, 2014
-=======
--- core questions: when did this happen? what properties and relations does the event have? what participants does it have?
--- events are process portions with participants: fields, objects, networks
--- conjecture: all events are movements at some granularity
--- properties and relations are limited to thematic ones (same as for objects)
--- temporal properties and relations defined here, as they only apply to events
--- the spatial properties and relations of events come from their participants (objects, for now)
--- also, spatial properties and relations come from making events a sub-class of objects (there is no reason not to, as they have all object behavior)
--- computing event outcomes: central, but how to specify? do simple examples
--- (c) Werner Kuhn
--- latest change: Nov 20, 2014
->>>>>>> FETCH_HEAD
 
 module Event where
 
@@ -31,18 +18,10 @@ import Object
 
 -- the class of all event types
 -- Eq captures identity
-<<<<<<< HEAD
 -- all participants are objects for now
 class (Eq event, OBJECT object) => EVENT event object where
 	when :: event -> Date -- timing the event
 	within :: event -> Period -- containing the event
-=======
--- all events are objects!
--- all participants are objects for now
-class (Eq event, OBJECTS event, OBJECTS object, LOCATE event object) => EVENTS event object where
-	within :: event -> Maybe Period -- any time interval containing the event
-	when :: event -> Maybe Period -- any time interval timing the event
->>>>>>> FETCH_HEAD
 	during :: event -> event -> Bool 
 	before :: event -> event -> Bool 
 	after :: event -> event -> Bool 
--- CoreConceptsHs/Object.hs
+++ CoreConceptsHs/Object.hs
@@ -2,19 +2,11 @@
 -- core concept: object 
 -- core questions: where is this object? what are its properties and relations? is it the same as that object?
 -- properties and relations are limited to thematic ones
-<<<<<<< HEAD
 -- all spatial properties and relations come from Location (only for other objects as grounds, so far)
 -- objects have no temporal properties or relations
 -- objects have state, queried through their spatial and thematic properties and relations
 -- (c) Werner Kuhn
 -- latest change: Nov 21, 2014
-=======
--- spatial properties and relations come from Location
--- objects have no temporal properties or relations
--- can we have something that corresponds to OGC's get capabilities, returning implemented properties and relations?
--- (c) Werner Kuhn
--- latest change: Nov 20, 2014
->>>>>>> FETCH_HEAD
 
 module Object where
 
@@ -22,18 +14,8 @@ import Location
 
 -- the class of all object types
 -- Eq captures identity
-<<<<<<< HEAD
 class (Eq object, LOCATED object object, POSITIONED object, BOUNDED object) => OBJECT object where
-=======
--- LOCATE captures spatial relations between objects (others may yet need to be instantiated)
-class (Eq object, LOCATE object object) => OBJECTS object where
->>>>>>> FETCH_HEAD
 	get :: (object -> value) -> object -> value
-	set :: (object -> value) -> object -> value -> object
 	is :: (object -> object -> Bool) -> object -> object -> Bool
-<<<<<<< HEAD
-=======
-	be :: (object -> object -> Bool) -> (object -> object) -> object -- needs testing and a decision on how to record relations in objects
->>>>>>> FETCH_HEAD
 	get property = property
 	is relation = relation
