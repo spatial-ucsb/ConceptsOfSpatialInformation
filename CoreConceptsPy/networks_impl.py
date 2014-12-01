8e8f9d25c33e08db0b12eb41c0e0853a11637869
Michel Zimmer <mzimmer@uni-bremen.de>
2014-12-01 11:27:11 -0800

    Added missing return statements

--- CoreConceptsPy/networks_impl.py
+++ CoreConceptsPy/networks_impl.py
@@ -18,34 +18,34 @@ class NetworksX(CcNetwork):
     """
     NetworksX wrapper implementation for core concept 'network'
     """
-    
+
     def __init__( self ):
         pass
 
     def nodes( self ):
         """ @return a copy of the graph nodes in a list """
-        self.nodes()
-    
+        return self.nodes()
+
     def edges( self ):
         """ @return list of edges """
-        self.edges()
-    
+        return self.edges()
+
     def addNode( self, n ):
         """ Add a single node n """
         self.add_node(n)
-    
+
     def addEdge( self, u, v ):
         """ Add an edge between u and v """
         self.add_edge(u, v)
-    
+
     def connected( self, u, v ):
         """ @return whether node v can be reached from node u """
-        raise len(self.shortestPath(u, v)) > 0
+        return len(self.shortestPath(u, v)) > 0
 
     def shortestPath( self, source, target ):
         """ @return shortest path in the graph """
         return nx.shortest_path(self, source, target)
-    
+
     def degree( self, n ):
         """ @return number of the nodes connected to the node n """
         return len(self.neighbors(n))
@@ -53,7 +53,7 @@ class NetworksX(CcNetwork):
     def distance( self, source, target ):
         """ @return the length of the shortest path from the source to the target """
         return nx.shortest_path_length(self, source, target)
-    
+
     def breadthFirst( self, node, cutoff ):
         """ @return all nodes within the distance cutoff from node in this network """
         return nx.single_source_shortest_path(self, node, cutoff)
