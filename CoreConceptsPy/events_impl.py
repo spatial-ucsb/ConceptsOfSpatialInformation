29be608dbe4962d2f31790d51326468e170b9164
Marc Thiemann <marctim2013@googlemail.com>
2014-12-01 09:41:23 -0800

    Event implementation
    Event Implementation for the initial core concept spec

--- CoreConceptsPy/events_impl.py
+++ CoreConceptsPy/events_impl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+from coreconcepts import AEvents, Event, Period
 
 """
  Module abstract goes here.
@@ -19,5 +19,74 @@ from utils import _init_log
 log = _init_log("networks_impl")
 
 class SomeConcreteEvents(AEvents):
-    """ Implementation """
-    pass
+
+	@staticmethod
+    def within( ev ):
+        """
+        @ev an event
+        @return a Period 
+        """
+        return ev.period
+    
+    @staticmethod
+    def when( ev ):
+        """
+        @ev an event
+        @return a Period 
+        """
+        return ev.period
+    
+    @staticmethod
+    def during( ev, otherEv ):
+        """
+        @ev an event
+        @ev otherEvent another event
+        @return boolean
+        """
+        return ev.getStartTime >= otherEv.getStartTime and ev.getEndTime <= otherEv.getEndTime
+    
+    @staticmethod
+    def before( ev, otherEv ):
+        """
+        @ev an event
+        @ev otherEvent another event
+        @return boolean
+        """
+        return ev.getEndTime < otherEv.getStartTime
+    
+    @staticmethod
+    def after( ev, otherEv ):
+        """
+        @ev an event
+        @ev otherEvent another event
+        @return boolean
+        """
+        return ev.getStartTime > otherEv.getEndTime
+    
+    @staticmethod
+    def overlap( ev, otherEv ):
+        """
+        @ev an event
+        @ev otherEvent another event
+        @return boolean
+        """
+        return (ev.getStartTime < otherEv.getStartTime and ev.getEndTime < otherEv.getEndTime and ev.getEndTime >= otherEv.getStartTime) or (ev.getStartTime > otherEv.getStartTime and ev.getStartTime <= otherEv.getEndTime and ev.getEndTime > otherEv.getEndTime)
+    
+class Event(object):
+    """ Simple event class. TODO: implement"""
+    
+    __init__(self, period):
+    	self.period = period
+
+    def getStartTime(self):
+    	return self.period.startTime
+
+    def getEndTime(self):
+    	return self.period.endTime
+
+class Period(object):
+    """ Simple period class. TODO: implement"""
+    
+    __init__(self, startTime, endTime):
+    	self.startTime = startTime
+    	self.endTime = endTime
