0049a96acac30b654deb14c84467e252b36629d4
Michel Zimmer <mzimmer@uni-bremen.de>
2014-12-01 11:56:01 -0800

    (Py) NetworkX: self -> self.G

--- CoreConceptsPy/networks_impl.py
+++ CoreConceptsPy/networks_impl.py
@@ -20,40 +20,40 @@ class NetworksX(CcNetwork):
     """
 
     def __init__( self ):
-        pass
+        self.G = nx.Graph()
 
     def nodes( self ):
         """ @return a copy of the graph nodes in a list """
-        return self.nodes()
+        return self.G.nodes()
 
     def edges( self ):
         """ @return list of edges """
-        return self.edges()
+        return self.G.edges()
 
     def addNode( self, n ):
         """ Add a single node n """
-        self.add_node(n)
+        self.G.add_node(n)
 
     def addEdge( self, u, v ):
         """ Add an edge between u and v """
-        self.add_edge(u, v)
+        self.G.add_edge(u, v)
 
     def connected( self, u, v ):
         """ @return whether node v can be reached from node u """
-        return len(self.shortestPath(u, v)) > 0
+        return len(self.G.shortestPath(u, v)) > 0
 
     def shortestPath( self, source, target ):
         """ @return shortest path in the graph """
-        return nx.shortest_path(self, source, target)
+        return nx.shortest_path(self.G, source, target)
 
     def degree( self, n ):
         """ @return number of the nodes connected to the node n """
-        return len(self.neighbors(n))
+        return len(self.G.neighbors(n))
 
     def distance( self, source, target ):
         """ @return the length of the shortest path from the source to the target """
-        return nx.shortest_path_length(self, source, target)
+        return nx.shortest_path_length(self.G, source, target)
 
     def breadthFirst( self, node, cutoff ):
         """ @return all nodes within the distance cutoff from node in this network """
-        return nx.single_source_shortest_path(self, node, cutoff)
+        return nx.single_source_shortest_path(self.G, node, cutoff)
