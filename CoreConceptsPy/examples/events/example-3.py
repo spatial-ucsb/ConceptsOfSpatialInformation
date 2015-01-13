9d94258088dba2625a5421758e3de155e6c82f17
Marc Thiemann <marctim2013@googlemail.com>
2015-01-12 16:23:06 -0800

    Implemented an event example using Earthquake Data

---
+++ CoreConceptsPy/examples/events/example-3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example 3:
+
+Use Cases:
+
+Provided data:
+
+"""
+
+__author__ = "Marc Tim Thiemann"
+__copyright__ = "Copyright 2014"
+__credits__ = ["Marc Tim Thiemann"]
+__license__ = ""
+__version__ = "0.1"
+__maintainer__ = ""
+__email__ = ""
+__date__ = "January 2015"
+__status__ = "Development"
+
+import sys
+
+sys.path = [ '.', '../..' ] + sys.path
+from utils import _init_log
+from events import *
+import dateutil.parser
+from datetime import *
+
+log = _init_log("example-3")
