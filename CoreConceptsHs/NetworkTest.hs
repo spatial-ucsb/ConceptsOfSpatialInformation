2a58ba6212b504801594caefebb4f3c59e3430c7
Michel Zimmer <mzimmer@uni-bremen.de>
2015-03-02 18:21:24 -0800

    Add: Showcase directed graphs

--- CoreConceptsHs/NetworkTest.hs
+++ CoreConceptsHs/NetworkTest.hs
@@ -2,7 +2,15 @@ module NetworkTest where
 
 import Network
 import NetworkImpl
-import Data.Graph.Inductive.Graph
+
+myGraph :: CCGraph
+myGraph = mkGraph [(1, "Node_1"), (2, "Node_2"), (3, "Node_3")] [(1, 2, "Edge_1_2"), (2, 3, "Edge_2_3"), (3, 1, "Edge_3_1")]
 
 main :: IO ()
-main = putStrLn $ prettify $ addEdge (addNode (addNode emptyCCGraph (1, "Node1")) (2, "Node2")) (1, 2, "Edge1-2")
+main = do
+	print $ nodes myGraph
+	print $ edges myGraph
+	putStrLn ""
+	print $ shortestPath myGraph (1, "") (2, "")
+	print $ shortestPath myGraph (2, "") (1, "")
+	print $ shortestPath (addEdge myGraph (2, 1, "Edge_2_1")) (2, "") (1, "")
