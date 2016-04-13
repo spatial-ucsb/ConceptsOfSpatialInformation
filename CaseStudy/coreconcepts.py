40be653b1522ebe164b4be394884f7155d521bce
saralafia <saralafia@users.noreply.github.com>
2016-04-12 17:51:30 -0700

    Added files via upload

---
+++ CaseStudy/coreconcepts.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+import os
+
+# TODO: refactor so that following arc implementations aren't called unless arc processing is needed
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+
+# Set ArcPy workspace settings
+env.workspace = "C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data" # TODO: set workspace determined by filepath locations
+workspace = env.workspace
+arcpy.env.overwriteOutput = True
+# Check out any necessary licenses
+arcpy.CheckOutExtension("spatial")
+
+
+##############################
+## Core Concepts classes ##
+
+
+class CcField(object):
+    """
+    Class defining abstract field.
+    Based on Field.hs
+    """
+    def __init__(self, filepath, geoObject):
+        self.filepath = filepath
+        self.domain = geoObject
+        # TODO: restrict value pairs to geoObject
+        pass
+
+
+    def value_at( self, position ):
+        """
+        @return the value of field at position, or None if it is outside of the domain.
+        """
+        # TODO: check if position falls within value
+        raise NotImplementedError("valueAt")
+
+
+    def domain( self ):
+        """
+        @return current domain of the field
+        """
+        raise NotImplementedError("domain")
+
+
+    def restrict_domain(self, object, operation ):
+        pass
+
+
+    def local( self, fields, fun ):
+        """
+        Uses raster calculator from ArcPy
+        TODO: make a general funtion for more than two fields
+        """
+        raise NotImplementedError("local")
+
+
+class CcObject(object):
+    """
+    Abstract class for core concept 'object'
+    Based on Object.hs
+    """
+    def __init__( self, filepath, objIndex, geoObject ):
+        self.filepath = filepath
+        self.sObj = objIndex
+        self.domain = geoObject
+
+
+    def bounds( self ):
+        raise NotImplementedError("bounds")
+
+
+    def relation( self, obj, relType ):
+        """ @return Boolean True if self and obj are in a relationship of type relType
+                    False otherwise
+        """
+        raise NotImplementedError("relation")
+
+
+    def property( self, prop ):
+        """
+        @param prop the property name
+        @return value of property in obj
+        """
+        raise NotImplementedError("property")
+
+
+    def identity( self, obj ):
+        """
+        @param an object
+        @return Boolean True if self and obj are identical
+        """
+        raise NotImplementedError("identity")
+
+
+class CcGranularity:
+    def __init__(self):
+        # TODO: cell_size_x, cell_size_y
+        raise NotImplementedError("CcGranularity")
+
+
+##############################
+## Core Concepts Subclasses ##
+
+
+class GeoTiffField(CcField):
+    """
+    Concrete class for core concept 'field'
+    For handling .tiff files
+    """
+    def __init__(self, filepath, geoObject):
+        super(GeoTiffField, self).__init__(filepath, geoObject) # TODO: check if syntax is correct for returning type (make sure __init__ is correct)
+        self.filepath = filepath
+        self.domain = geoObject
+        self.filename = os.path.basename(filepath)
+
+
+    def value_at(self, value_at):
+        # TODO: return value at current position (must integrate with other operations)
+        raise NotImplementedError("value at")
+
+
+    def restrict_domain(self, object, operation ):
+        """
+        Restricts current instance's domain based on object's domain
+        @param object an object to be subtracted to the current domain
+        @param operation an operation to be performed based on the object
+        """
+
+        (nfilepath, nfilename) = os.path.split(self.filepath)
+        outputLocation = "-"
+
+        if operation == 'inside':
+            ## arcpy.env.snapRaster = self.filepath #TODO: resolve output shift
+            # extract by mask
+            output = arcpy.sa.ExtractByMask(self.filename, object.filename)
+            # determine save credentials
+            outputLocation = nfilepath + "\_masked_" + nfilename
+            output.save(outputLocation)
+
+        elif operation == 'outside':
+            # "preprocessing" temporary 'fix' to create China w/o gas flares (needed for Extract By Mask)
+            newChina = self.removeFlares(object)
+
+            print("output location2 ", newChina)
+            # extract by mask
+            output = arcpy.sa.ExtractByMask(self.filename, newChina)
+            # determine save credentials
+            outputLocation = nfilepath + "\_erased_" + nfilename
+            output.save(outputLocation)
+            print("output location ", outputLocation)
+
+            ## raise NotImplementedError("restrict domain 'outside'")
+            ## pass
+
+        # update cc instance's attributes
+        desc = arcpy.Describe(outputLocation)
+        self.domain = desc.extent
+        self.filepath = outputLocation
+        self.filename = os.path.basename(outputLocation)
+
+        print("self type: ", type(self))
+
+        return self
+
+    # TODO: remove; temporary fix to "preprocess"; see restrict_domain
+    def removeFlares(self, object):
+        outputLocation = "C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data\China_noFlares.shp"
+        # erase gas flares from country, generates a mask
+        arcpy.Erase_analysis("C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data\China.shp", object.filename, outputLocation)
+
+        return outputLocation
+
+
+    def local( self, fields, operation ): # TODO: fields is a list, make it one
+        """
+        Uses raster calculator from ArcPy
+        @param fields a field to be subtracted to the current domain
+        @param operation an operation to be performed based on the field
+        """
+        if operation == 'average':
+            # perform averaging operation # TODO: generalize funtion for more than two fields
+            output = (Float(self.filepath)+ Float(fields.filepath))/2
+            # determine save credentials
+            (nfilepath, nfilename) = os.path.split(self.filepath)
+            outputLocation = nfilepath + "\_averaged" + nfilename
+            output.save(outputLocation)
+
+            # and update cc instance's attributes
+            desc = arcpy.Describe(outputLocation)
+            self.domain = desc.extent
+            self.filepath = outputLocation
+            self.filename = os.path.basename(outputLocation)
+
+            print("self.domain ", self.domain)
+            print("self.filepath ", self.filepath)
+            print("self.filename ", self.filename)
+        elif operation == 'maximum':
+            #TODO complete list of local operations
+            #output = (Float(self.filepath)+Float(fields.filepath))/2
+            raise NotImplementedError("local 'maximum'")
+
+        else:
+            print 'the input function is not defined'
+
+        return self
+
+
+    def coarsen( self, cellW, cellH ):
+        """
+        Uses resample from Data Management in ArcPy
+        @param cellW cell width
+        @param callH cell height
+        """
+        (nfilepath, nfilename) = os.path.split(self.filepath)
+        outputLocation = nfilepath + "\_coarsened" + nfilename
+
+        concatCellSize = str(cellW) + " " + str(cellH)
+
+        arcpy.Resample_management(self.filename, outputLocation, concatCellSize)
+
+        # and update cc instance's attributes
+        desc = arcpy.Describe(outputLocation)
+        self.domain = desc.extent
+        self.filepath = outputLocation
+        self.filename = os.path.basename(outputLocation)
+
+        return self
+
+class ArcShpObject(CcObject):
+    """
+    Concrete class for core concept 'object'
+    For handling .shp files
+    """
+    def __init__( self, filepath, objIndex, domain ):
+        super(ArcShpObject, self).__init__(filepath, objIndex, domain)
+        self.filepath = filepath
+        self.sObj = objIndex
+        self.domain = domain
+        self.filename = os.path.basename(filepath)
+
+    def buffer ( self, distance, unitType ):
+        """
+        Buffer input object
+        @param distance a distance extent to buffer
+        @param unitType unit type (ie. decimal degrees, feet)
+        """
+
+        (nfilepath, nfilename) = os.path.split(self.filepath)
+        outputLocation = nfilepath + "\_buffered_" + nfilename
+
+        concatDistance = str(distance) + " " + unitType
+        arcpy.Buffer_analysis(self.filename, outputLocation, concatDistance)
+
+        # update cc instance's attributes
+        desc = arcpy.Describe(outputLocation)
+        self.domain = desc.extent
+        self.filepath = outputLocation
+        self.filename = os.path.basename(outputLocation)
+
+        return self
+
+
+
+#######################
+## Utility functions ##
+
+
+# make CcField instance (factory)
+def makeField(filepath):
+    domain = determineDomain(filepath) # determine domain
+    # determine input file type
+    if filepath.endswith(".tif"):
+        return GeoTiffField(filepath, domain)
+    elif filepath.endswith(".mp3"):
+        pass
+    assert 0, "Bad shape creation: " + filepath
+    #makeField = staticmethod(makeField)
+
+
+# make CcObject instance (factory)
+def makeObject(filepath):
+    domain = determineDomain(filepath) # determine domain
+    # determine input file type
+    if filepath.endswith(".shp"):
+        return ArcShpObject(filepath, 1, domain) #TODO: alter objIndex
+    elif filepath.endswith(".mp3"):
+        pass
+    assert 0, "Bad shape creation: " + filepath
+    #makeObject = staticmethod(makeObject)
+
+
+# helper function for cc factories
+def determineDomain(filepath):
+    desc = arcpy.Describe(filepath)
+    return desc.extent
+
+# TODO: Integrate geoEvent & other cc elements into instance creation
+# TODO: Refactor code via methods mentioned in other TODOs (separate by file; determine imports)
---
+++ CaseStudy/testingChinaLights.py
@@ -0,0 +1,35 @@
+__author__ = ""
+__date__ = ""
+__status__ = ""
+
+from coreconcepts import * # TODO: Package up Cc
+
+china_boundary_filepath = "C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data\China.shp"
+china_lights_1_filepath = "C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data\F101994.tif"
+china_lights_2_filepath = "C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data\F121994.tif"
+gas_flares_filepath     = "C:\Users\lafia\Documents\GitHub\ConceptsOfSpatialInformation\CoreConceptsPy\ArcPy\data\Flares_China_1.shp"
+china_roads_filepath    = "data/china_roads.shp"
+
+# TODO: determine how to chain following methods when different objects return (syntax adjustment?)
+
+# create fields & objects from file path
+china_boundary = makeObject(china_boundary_filepath)
+china_lights_1 = makeField(china_lights_1_filepath).restrict_domain(china_boundary, 'inside')
+china_lights_2 = makeField(china_lights_2_filepath).restrict_domain(china_boundary, 'inside')
+gas_flares = makeObject(gas_flares_filepath)
+
+# average fields
+average_luminosity = china_lights_1.local(china_lights_2, 'average')
+
+# remove gas flares
+luminosity = average_luminosity.restrict_domain(gas_flares, 'outside')
+
+# create roads buffer
+roads = makeObject(china_roads_filepath)
+roads_buffered = roads.buffer(0.5, 'DecimalDegrees') # TODO: updated function calling convention (exclude object)
+
+# restrict domain of luminosity to road buffer
+luminosity_around_roads = luminosity.restrict_domain(roads_buffered, 'inside')
+
+# aggregate previous information
+results = luminosity_around_roads.coarsen(0.1, 0.1)
